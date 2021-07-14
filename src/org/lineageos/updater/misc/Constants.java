PixysOS/packages_apps_Updater

Updater: Add device maintainer name
Change-Id: I3a895ef65a0997a51434147f22de12f403d795d5

src/org/lineageos/updater/misc/Constants.java
@@ -51,4 +51,6 @@ public final class Constants {
     public static final String PREF_INSTALL_PACKAGE_PATH = "install_package_path";
     public static final String PREF_INSTALL_AGAIN = "install_again";
     public static final String PREF_INSTALL_NOTIFIED = "install_notified";
+
+    public static final String PROP_MAINTAINER = "ro.pixys.maintainer";
 }

kota69th/android_packages_apps_Updater

Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

src/org/lineageos/updater/misc/Constants.java
@@ -54,4 +54,6 @@ public final class Constants {
 
     public static final String UPDATE_RECOVERY_EXEC = "/vendor/bin/install-recovery.sh";
     public static final String UPDATE_RECOVERY_PROPERTY = "persist.vendor.recovery_update";
+
+    public static final String PROP_MAINTAINER = "ro.cherish.maintainer";
 }